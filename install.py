fc795cc0b90448b8ebf1b35e731bcab086e2af29
Benjamin <b.oechsle@web.de>
2021-11-20 16:09:26 +0100

    added proper home

--- install.py
+++ install.py
@@ -1,7 +1,8 @@
 # install script for this nvim on arch linux (NOT on windows)
 import os
+home = os.path.expanduser("~")
 os.system("yay -R --noconfirm neovim")
 os.system("yay -S --noconfirm neovim-git nvim-packer-git")
-if os.path.isdir("~/.config/nvim"):
-    os.system("mv ~/.config/nvim ~/.config/nvimBACKUP")
-os.system("mv ../nvim ~/.config")
+if os.path.isdir(home + "/.config/nvim"):
+    os.system("mv " + home + "/.config/nvim " + home + "/.config/nvimBACKUP")
+os.system("mv ../nvim " + home + "/.config")
