727815710f3760882e90bd6af99ec402e7450ab2
Benjamin <b.oechsle@web.de>
2021-11-20 15:50:38 +0100

    changed script

--- install.py
+++ install.py
@@ -1,5 +1,6 @@
 # install script for this nvim on arch linux (NOT on windows)
 import os
+os.system("yay -R --noconfirm neovim")
 os.system("yay -S --noconfirm neovim-git nvim-packer-git")
 if os.path.isdir("~/.config/nvim"):
     os.system("mv ~/.config/nvim ~/.config/nvimBACKUP")
