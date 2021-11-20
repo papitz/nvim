b4329a4d090f663429a315273a8d33bbda4b30fc
Benjamin <b.oechsle@web.de>
2021-11-20 16:30:10 +0100

    prettyprint

--- install.py
+++ install.py
@@ -1,6 +1,14 @@
 # install script for this nvim on arch linux (NOT on windows)
 import os
 home = os.path.expanduser("~")
+print(
+    "██╗    ██╗██╗    ██╗ ██████╗████████╗██╗   ██╗██╗███╗   ███╗",
+    "██║    ██║██║    ██║██╔════╝╚══██╔══╝██║   ██║██║████╗ ████║",
+    "██║ █╗ ██║██║ █╗ ██║██║        ██║   ██║   ██║██║██╔████╔██║",
+    "██║███╗██║██║███╗██║██║        ██║   ╚██╗ ██╔╝██║██║╚██╔╝██║",
+    "╚███╔███╔╝╚███╔███╔╝╚██████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║",
+    " ╚══╝╚══╝  ╚══╝╚══╝  ╚═════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝",
+)
 os.system("yay -R --noconfirm neovim")
 os.system("yay -S --noconfirm neovim-git nvim-packer-git")
 if os.path.isdir(home + "/.config/nvim"):
