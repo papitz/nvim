2e2d840834a97b5905616233f4dc139997570003
papitz <paul@familie-hoeppner.de>
2021-11-30 10:06:54 +0100

    fixed the tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,11 +7,8 @@ local tree_cb = require("nvim-tree.config").nvim_tree_callback
 -- Set alias for vim.g.
 local g = vim.g
 
-g.nvim_tree_ignore = { ".git", "node_modules", ".cache", "__pycache__" } -- Ignore these types in listing.
-g.nvim_tree_auto_ignore_ft = { "dashboard" } -- Don't open tree on specific fiypes.
 g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened.
 g.nvim_tree_indent_markers = 1 -- This option shows indent markers when folders are open.
-g.nvim_tree_hide_dotfiles = 1 -- This option hides files and folders starting with a dot `.`.
 g.nvim_tree_git_hl = 1 -- Will enable file highlight for git attributes (can be used without the icons).
 g.nvim_tree_highlight_opened_files = 1 -- Will enable folder and file icon highlight for opened files/directories.
 g.nvim_tree_add_trailing = 0 -- Append a trailing slash to folder names. ]]
@@ -36,6 +33,10 @@ nvimtree.setup({
 			warning = "",
 			error = "",
 		},
+	},
+	filters = {
+		dotfiles = true,
+		custom = { ".git", "node_modules", ".cache", "__pycache__" },
 	},
 	view = {
 		width = "15%",
