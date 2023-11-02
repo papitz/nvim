7447a4cb9cfb04dac6dcb702bab3a9223da0d8eb
papitz <paul@familie-hoeppner.de>
2023-11-02 16:04:04 -0700

    Removed notify animations

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -2,7 +2,9 @@ local notify = require("notify")
 vim.notify = notify
 
 notify.setup({
-	stages = "fade",
-	timeout = 2000,
-	max_width = 50,
+    stages = "static",
+    timeout = 2000,
+    max_height = function() return math.floor(vim.o.lines * 0.75) end,
+    max_width = function() return math.floor(vim.o.columns * 0.75) end,
+    on_open = function(win) vim.api.nvim_win_set_config(win, {zindex = 100}) end
 })
