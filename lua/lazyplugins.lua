f7a5976c5e5563ad5c7dddbffe18224e18b63c7c
papitz <paul@familie-hoeppner.de>
2024-01-19 09:53:46 +0100

    Started changing plugin specs

--- lua/lazyplugins.lua
+++ lua/lazyplugins.lua
@@ -1,22 +1,22 @@
 local plugins = {
     -- quicker lua loading
     {"lewis6991/impatient.nvim"}, -- color scheme
-    {"navarasu/onedark.nvim", lazy = false, priority = 1000},
-    {"folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {}}, {
-        "epwalsh/obsidian.nvim",
-        lazy = true,
-        -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand':
-        event = {"BufReadPre " .. vim.fn.expand("~") .. "/notes/**.md"},
-        dependencies = {
-            -- Required.
-            "nvim-lua/plenary.nvim", -- Optional, for completion.
-            "hrsh7th/nvim-cmp",
-
-            -- Optional, for search and quick-switch functionality.
-            "nvim-telescope/telescope.nvim"
-        },
-        config = function() require("plugins/obsidian") end
-    }, -- { "github/copilot.vim" },
+    -- {"navarasu/onedark.nvim", lazy = false, priority = 1000},
+    -- {"folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {}}, {
+    --     "epwalsh/obsidian.nvim",
+    --     lazy = true,
+    --     -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand':
+    --     event = {"BufReadPre " .. vim.fn.expand("~") .. "/notes/**.md"},
+    --     dependencies = {
+    --         -- Required.
+    --         "nvim-lua/plenary.nvim", -- Optional, for completion.
+    --         "hrsh7th/nvim-cmp",
+    --
+    --         -- Optional, for search and quick-switch functionality.
+    --         "nvim-telescope/telescope.nvim"
+    --     },
+    --     config = function() require("plugins/obsidian") end
+    -- }, -- { "github/copilot.vim" },
     -- File explorer tree.
     {
         "nvim-neo-tree/neo-tree.nvim",
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -172,3 +172,13 @@ require("neo-tree").setup({
         }
     }
 })
+
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim"
+    },
+    config = function() require("plugins/neo-tree") end
+}
