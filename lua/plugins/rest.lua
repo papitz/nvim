b7b4a862ee5ef15995bd50fe47d11802fcf8bd6d
papitz <paul@familie-hoeppner.de>
2024-02-01 11:56:28 +0100

    Added rest plugin

---
+++ lua/plugins/rest.lua
@@ -0,0 +1,9 @@
+require("util").nmap("<leader>b", "<Plug>RestNvim")
+return {
+    "rest-nvim/rest.nvim",
+    dependencies = {{"nvim-lua/plenary.nvim"}},
+    ft = {'http'},
+    config = function()
+        require("rest-nvim").setup({highlight = {enabled = false, timeout = 150}})
+    end
+}
