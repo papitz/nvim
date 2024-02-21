88c4e81c1e1173eee5ae955d589c16eef6d81c72
papitz <paul@familie-hoeppner.de>
2024-02-21 21:36:13 +0100

    Added img clip plugin

---
+++ lua/plugins/img-clip.lua
@@ -0,0 +1,12 @@
+return {
+  "HakonHarnes/img-clip.nvim",
+  cmd = "PasteImage",
+  opts = {
+    -- add options here
+    -- or leave it empty to use the default settings
+  },
+  keys = {
+    -- suggested keymap
+    { "<leader>ip", "<cmd>PasteImage<cr>", desc = "Paste clipboard image" },
+  },
+}
