239fd0b695d96568a4b38c2619e7ff30cf66d97f
papitz <paul@familie-hoeppner.de>
2024-04-11 17:37:35 +0200

    Added filetype configs

--- ftplugin/dart.lua
+++ ftplugin/dart.lua
@@ -1,5 +1,5 @@
 -- Defining alias for vim.opt.
-local opt = vim.opt
+local opt = vim.opt_local
 HOME = os.getenv("HOME")
 opt.tabstop = 2
 opt.softtabstop = 2 -- set tab size
--- ftplugin/json.lua
+++ ftplugin/json.lua
@@ -1,2 +1,5 @@
 local opt = vim.opt_local
 opt.foldmethod = "indent"
+opt.tabstop = 2
+opt.softtabstop = 2 -- set tab size
+opt.shiftwidth = 2 -- affect amount of indentation
