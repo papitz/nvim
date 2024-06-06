80b4cf9a70095a5c07445315427a6e2997aa6564
papitz <paul@familie-hoeppner.de>
2024-06-06 14:54:05 +0200

    Replaced vim-surround with mini-surround

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,5 +1,5 @@
 return {
-  {'windwp/nvim-autopairs', opts = {}}, -- This is for html and it can autorename too!
-  {'windwp/nvim-ts-autotag'}, {'andymass/vim-matchup'}, {'tpope/vim-surround'}
+	{ 'windwp/nvim-autopairs', opts = {} }, -- This is for html and it can autorename too!
+	{ 'windwp/nvim-ts-autotag' },
+	{ 'andymass/vim-matchup' },
 }
-
