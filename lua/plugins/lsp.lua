8d814ed926ba9813a30710d77ae8a464be3cee36
papitz <paul@familie-hoeppner.de>
2024-06-20 13:18:49 +0200

    Added yazi and updated neodev to lazydev

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,7 @@
 -- disable virtual text in Latex documents FIX for overfull hbox madness
 vim.cmd(
-    [[autocmd Filetype tex lua vim.g.diagnostic_virtual_text = false; vim.diagnostic.config({ virtual_text = vim.g.diagnostic_virtual_text })]])
+	[[autocmd Filetype tex lua vim.g.diagnostic_virtual_text = false; vim.diagnostic.config({ virtual_text = vim.g.diagnostic_virtual_text })]]
+)
 -- show the diagnostic description when the cursor is placed on the error
 -- vim.cmd([[autocmd CursorHold * lua vim.diagnostic.open_float(nil,{focusable=false,scope="cursor"})]])
 -- vim.cmd([[autocmd CursorHold * Lspsaga show_cursor_diagnostics]])
@@ -17,101 +18,126 @@ vim.cmd(
 -- end
 
 return {
-  {
-    'williamboman/mason.nvim',
-    opts = {
-      max_concurrent_installers = 10,
-      -- log_level = vim.log.levels.TRACE,
-      ui = {
-        icons = {
-          package_installed = '✓',
-          package_pending = '➜ ',
-          package_uninstalled = '✗'
-        }
-      }
-    }
-  }, {
-    'pmizio/typescript-tools.nvim',
-    ft = {'javascript', 'typescript'},
-    opts = {},
-    dependencies = {'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig'}
-  }, {
-    'williamboman/mason-lspconfig.nvim',
-    dependencies = {
-      'neovim/nvim-lspconfig', 'folke/neodev.nvim', 'williamboman/mason.nvim'
-      -- TODO: Check if we might need this
-      -- , "hrsh7th/nvim-cmp"
-    },
-    opts = {ensure_installed = {'lua_ls'}},
-    config = function()
-      -- Nvim specific stuff
-      require('neodev').setup({
-        -- For testing library
-        library = {plugins = {'neotest'}, types = true}
-      })
+	{
+		'williamboman/mason.nvim',
+		opts = {
+			max_concurrent_installers = 10,
+			-- log_level = vim.log.levels.TRACE,
+			ui = {
+				icons = {
+					package_installed = '✓',
+					package_pending = '➜ ',
+					package_uninstalled = '✗',
+				},
+			},
+		},
+	},
+	{
+		{
+			'folke/lazydev.nvim',
+			ft = 'lua', -- only load on lua files
+			opts = {
+				library = {
+					-- See the configuration section for more details
+					-- Load luvit types when the `vim.uv` word is found
+					{ path = 'luvit-meta/library', words = { 'vim%.uv' } },
+				},
+			},
+		},
+		{ 'Bilal2453/luvit-meta', lazy = true }, -- optional `vim.uv` typings
+		{ -- optional completion source for require statements and module annotations
+			'hrsh7th/nvim-cmp',
+			opts = function(_, opts)
+				opts.sources = opts.sources or {}
+				table.insert(opts.sources, {
+					name = 'lazydev',
+					group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+				})
+			end,
+		},
+	},
+	{
+		'pmizio/typescript-tools.nvim',
+		ft = { 'javascript', 'typescript' },
+		opts = {},
+		dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+	},
+	{
+		'williamboman/mason-lspconfig.nvim',
+		dependencies = {
+			'neovim/nvim-lspconfig',
+			'williamboman/mason.nvim',
+			-- TODO: Check if we might need this
+			-- , "hrsh7th/nvim-cmp"
+		},
+		opts = { ensure_installed = { 'lua_ls' } },
+		config = function()
+			local deps_ok, lspconfig, util, cmp_lsp = pcall(function()
+				return require('lspconfig'), require('lspconfig.util'), require('cmp_nvim_lsp')
+			end)
+			if not deps_ok then
+				return
+			end
 
-      local deps_ok, lspconfig, util, cmp_lsp = pcall(function()
-        return require('lspconfig'), require('lspconfig.util'),
-               require('cmp_nvim_lsp')
-      end)
-      if not deps_ok then return end
+			local capabilities
+			do
+				local default_capabilities = vim.lsp.protocol.make_client_capabilities()
+				capabilities = {
+					textDocument = {
+						completion = { completionItem = { snippetSupport = true } },
+						codeAction = {
+							resolveSupport = {
+								properties = vim.list_extend(
+									default_capabilities.textDocument.codeAction.resolveSupport.properties,
+									{ 'documentation', 'detail', 'additionalTextEdits' }
+								),
+							},
+						},
+					},
+				}
+			end
 
-      local capabilities
-      do
-        local default_capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities = {
-          textDocument = {
-            completion = {completionItem = {snippetSupport = true}},
-            codeAction = {
-              resolveSupport = {
-                properties = vim.list_extend(
-                    default_capabilities.textDocument.codeAction.resolveSupport
-                        .properties,
-                    {'documentation', 'detail', 'additionalTextEdits'})
-              }
-            }
-          }
-        }
-      end
+			util.default_config = vim.tbl_deep_extend('force', util.default_config, {
+				capabilities = vim.tbl_deep_extend(
+					'force',
+					vim.lsp.protocol.make_client_capabilities(),
+					cmp_lsp.default_capabilities(capabilities)
+				),
+			})
 
-      util.default_config = vim.tbl_deep_extend('force', util.default_config, {
-        capabilities = vim.tbl_deep_extend('force', vim.lsp.protocol
-                                               .make_client_capabilities(),
-                                           cmp_lsp.default_capabilities(
-                                               capabilities))
-      })
+			require('mason-lspconfig').setup({ ensure_installed = { 'lua_ls' } })
+			-- require("mason-lspconfig").setup()
 
-      require('mason-lspconfig').setup({ensure_installed = {'lua_ls'}})
-      -- require("mason-lspconfig").setup()
-
-      require('mason-lspconfig').setup_handlers({
-        function(server_name) lspconfig[server_name].setup({}) end,
-        ['lua_ls'] = function()
-          lspconfig.lua_ls.setup({
-            settings = {Lua = {diagnostics = {globals = {'P'}}}}
-          })
-        end,
-        -- ["ltex"] = function()
-        --     lspconfig.ltex.setup({
-        --         on_attach = function(client, bufnr)
-        --             require("ltex_extra").setup({
-        --                 load_langs = {"de-DE", "en-US"}, -- table <string> : languages for which dictionaries will be loaded
-        --                 init_check = true, -- boolean : whether to load dictionaries on startup
-        --                 path = nil, -- string : path to store dictionaries. Relative path uses current working directory
-        --                 log_level = "none" -- string : "none", "trace", "debug", "info", "warn", "error", "fatal"
-        --             })
-        --         end,
-        --         -- Disable for markdown for now 15.11.23
-        --         -- filetype = { "bib", "gitcommit", "markdown", "org", "plaintex", "rst", "rnoweb", "tex", "pandoc", "quarto", "rmd" },
-        --         filetype = {"tex"},
-        --         settings = {ltex = {language = "de-DE"}}
-        --         -- settings = {ltex = {language = "en-US"}}
-        --     })
-        -- end,
-        ['eslint'] = function()
-          lspconfig.eslint.setup({settings = {format = true}})
-        end,
-      })
-    end
-  }
+			require('mason-lspconfig').setup_handlers({
+				function(server_name)
+					lspconfig[server_name].setup({})
+				end,
+				['lua_ls'] = function()
+					lspconfig.lua_ls.setup({
+						settings = { Lua = { diagnostics = { globals = { 'P' } } } },
+					})
+				end,
+				-- ["ltex"] = function()
+				--     lspconfig.ltex.setup({
+				--         on_attach = function(client, bufnr)
+				--             require("ltex_extra").setup({
+				--                 load_langs = {"de-DE", "en-US"}, -- table <string> : languages for which dictionaries will be loaded
+				--                 init_check = true, -- boolean : whether to load dictionaries on startup
+				--                 path = nil, -- string : path to store dictionaries. Relative path uses current working directory
+				--                 log_level = "none" -- string : "none", "trace", "debug", "info", "warn", "error", "fatal"
+				--             })
+				--         end,
+				--         -- Disable for markdown for now 15.11.23
+				--         -- filetype = { "bib", "gitcommit", "markdown", "org", "plaintex", "rst", "rnoweb", "tex", "pandoc", "quarto", "rmd" },
+				--         filetype = {"tex"},
+				--         settings = {ltex = {language = "de-DE"}}
+				--         -- settings = {ltex = {language = "en-US"}}
+				--     })
+				-- end,
+				['eslint'] = function()
+					lspconfig.eslint.setup({ settings = { format = true } })
+				end,
+			})
+		end,
+	},
 }
---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,27 @@
+return {
+  "mikavilpas/yazi.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  event = "VeryLazy",
+  keys = {
+    {
+      "<leader>-",
+      function()
+        require("yazi").yazi()
+      end,
+      desc = "Open the file manager",
+    },
+    {
+      -- Open in the current working directory
+      "<leader>cw",
+      function()
+        require("yazi").yazi(nil, vim.fn.getcwd())
+      end,
+      desc = "Open the file manager in nvim's working directory" ,
+    },
+  },
+  opts = {
+    open_for_directories = false,
+  },
+}
