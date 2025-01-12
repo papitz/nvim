15364fe516e1490e66b09f4b1c040107b070bff7
papitz <paul@familie-hoeppner.de>
2025-01-12 14:45:53 +0100

    Blink updates

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,5 +1,15 @@
 return {
-	{ 'windwp/nvim-autopairs', opts = {} }, -- This is for html and it can autorename too!
-	{ 'windwp/nvim-ts-autotag' },
+	{ 'windwp/nvim-autopairs', config = true, event = 'InsertEnter' }, -- This is for html and it can autorename too!
+	{
+		'windwp/nvim-ts-autotag',
+		opts = {
+			opts = {
+				-- Defaults
+				enable_close = true, -- Auto close tags
+				enable_rename = true, -- Auto rename pairs of tags
+				enable_close_on_slash = false, -- Auto close on trailing </
+			},
+		},
+	},
 	{ 'andymass/vim-matchup' },
 }
