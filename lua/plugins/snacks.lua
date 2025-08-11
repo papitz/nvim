4fce49d97f5218a760fcaad0f4049b97d0d6c70e
papitz <paul@familie-hoeppner.de>
2025-08-11 12:22:39 +0200

    Fixed ui select with a dedicated picker

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+-- lazy.nvim
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    picker = {
+      ui_select = true
+      -- your picker configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  }
+}
